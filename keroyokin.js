e43a4a56d2aa51fb364074e4ebdb4c991212f034
BOB <ivananggono@gmail.com>
2021-07-01 16:14:54 +0700

    initial actions

---
+++ keroyokin.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require ("express");
+const app = express();
+const port = 3000;
+
+const router = require("./routers")
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", router)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
